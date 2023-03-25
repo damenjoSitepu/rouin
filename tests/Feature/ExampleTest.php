1db2361c15adf045e7f00aa99cb40991a390f2ff
Damenjo Sitepu <damenjos@thebigrichgroup.com>
2023-03-25 14:15:34 +0700

    Update: Configure PHP Unit Tests

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php 
+
+namespace damenjositepu\Rouin\Tests;
+
+use Orchestra\Testbench\TestCase;
+
+class ExampleTest extends TestCase {
+    /** @test */
+    public function firstTest()
+    {
+        $this->assertEquals(true,true,'Failed!');
+        $this->assertEquals(true,true,'Failed!');
+    }
+}
